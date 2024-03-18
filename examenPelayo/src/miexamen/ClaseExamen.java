5cec16ebbb8693452daf92a2d7b87cfd1e69f531
pelayovv20 <pelayovv20@educastur.es>
2024-03-18 12:57:28 +0100

    Subida proyecto inicial

--- examenPelayo/src/miexamen/ClaseExamen.java
+++ examenPelayo/src/miexamen/ClaseExamen.java
@@ -4,7 +4,7 @@ package miexamen;
 /**
  * <h1>Entornos de desarrollo UT3</h1>
  * <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo</p>
- * @see <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcVOxDqyOeR35aU" target="_blank">Curso de Git y github de píldoras informáticas</a>
+ * @see <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU" target="_blank">Curso de Git y github de píldoras informáticas</a>
  * @author Pelayo Vázquez Vidal
  * @version 1.0.0
  * @since 18/03/2024
