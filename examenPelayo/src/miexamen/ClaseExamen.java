3f822341dff9bdfdb60b88cdb9365a185fd6e6e6
pelayovv20 <pelayovv20@educastur.es>
2024-03-18 12:31:42 +0100

    Añadidos los primeros ficheros al proyecto

---
+++ examenPelayo/src/miexamen/ClaseExamen.java
@@ -0,0 +1,12 @@
+
+package miexamen;
+
+
+public class ClaseExamen {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Estoy haciendo el examen de la UT3");
+    }
+    
+}
