0a55cebc8f9e3baa0aeeba6f9bcdaa9e5707578d
pelayovv20 <pelayovv20@educastur.es>
2024-03-18 12:47:23 +0100

    Añadidos comentarios de documentación en ClaseExamen

--- examenPelayo/src/miexamen/ClaseExamen.java
+++ examenPelayo/src/miexamen/ClaseExamen.java
@@ -1,12 +1,27 @@
 
 package miexamen;
 
+/**
+ * <h1>Entornos de desarrollo UT3</h1>
+ * <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo</p>
+ * @see <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcVOxDqyOeR35aU" target="_blank">Curso de Git y github de píldoras informáticas</a>
+ * @author Pelayo Vázquez Vidal
+ * @version 1.0.0
+ * @since 18/03/2024
+ */
+
+
+
 
 public class ClaseExamen {
     
     public static void main(String[] args) {
         
         System.out.println("Estoy haciendo el examen de la UT3");
+        
+        
+        
+ 
     }
     
 }
